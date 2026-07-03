0ee6fdd95669880aefeeec98df96ba5784762da4
siuzaziuza <aangheldrugarin@gmail.com>
2026-07-03 14:26:20 +0300

    Add startQuiz function to display quiz question

---
+++ script.js
@@ -0,0 +1,12 @@
+function startQuiz() {
+    document.getElementById("quiz").innerHTML = `
+        <h2>Prima întrebare</h2>
+
+        <p>Care este materia ta preferată?</p>
+
+        <button>Matematică</button>
+        <button>Informatică</button>
+        <button>Biologie</button>
+        <button>Istorie</button>
+    `;
+}
