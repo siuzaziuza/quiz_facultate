b04e2792a07441c605da283d1fe26d43764945fd
siuzaziuza <aangheldrugarin@gmail.com>
2026-07-03 14:52:03 +0300

    Implement quiz question display functionality

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
+let currentQuestion = 0;
+
+const questions = [
+{
+    question: "Ce activitate îți place cel mai mult?",
+    answers: [
+        "Rezolv probleme logice",
+        "Ajut oamenii",
+        "Desenez sau creez",
+        "Coordonez echipe",
+        "Lucrez cu bani"
+    ]
+}
+];
+
 function startQuiz() {
-    document.getElementById("quiz").innerHTML = `
-        <h2>Prima întrebare</h2>
 
-        <p>Care este materia ta preferată?</p>
+const quiz = document.getElementById("quiz");
 
-        <button>Matematică</button>
-        <button>Informatică</button>
-        <button>Biologie</button>
-        <button>Istorie</button>
-    `;
+quiz.innerHTML = `
+<h2>${questions[currentQuestion].question}</h2>
+
+<button>${questions[currentQuestion].answers[0]}</button><br><br>
+
+<button>${questions[currentQuestion].answers[1]}</button><br><br>
+
+<button>${questions[currentQuestion].answers[2]}</button><br><br>
+
+<button>${questions[currentQuestion].answers[3]}</button><br><br>
+
+<button>${questions[currentQuestion].answers[4]}</button>
+
+`;
+
 }
